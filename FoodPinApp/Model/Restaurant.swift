eb3fe0a53e23ddf8d7e6584223294eb173e43257
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-08-09 12:03:16 +0200

    Add Model

---
+++ FoodPinApp/Model/Restaurant.swift
@@ -0,0 +1,28 @@
+//
+//  Restaurant.swift
+//  FoodPinApp
+//
+//  Created by Adnann Muratovic on 09.08.21.
+//
+
+import Foundation
+
+
+class Restaurant {
+    var name = ""
+    var type = ""
+    var location = ""
+    var image = ""
+    var isVisited = false
+    var phone = ""
+    var rating = ""
+    
+    init(name: String, type: String, location: String, phone: String, image: String, isVisited: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.phone = phone
+        self.image = image
+        self.isVisited = isVisited
+    }
+}
